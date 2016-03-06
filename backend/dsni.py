87c1ee0441fec1002f455a6aeeb4bcd4f6ea5e57
Michael J. Kazin <mkazin@gmail.com>
2016-03-06 12:16:44 -0500

    Started work on backend.  Merging to master repo.

--- backend/dsni.py
+++ backend/dsni.py
@@ -1,18 +1,47 @@
 from flask import Flask, render_template, request, url_for, redirect
 import os
 import itertools
+import sqlite3
 from flask import Flask, render_template, request
-from modules.MbtaAPI import MbtaAPI
+# from modules.MbtaAPI import MbtaAPI
 app = Flask(__name__)
+
+
+def get_db_conn():
+    conn = sqlite3.connect('diplodocus.db')
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
     if request.method == "GET":
-        stops = MbtaAPI({'verbose': True}).getRoutes(70)
+        # stops = MbtaAPI({'verbose': True}).getRoutes(70)
+        stops = ['Hello', 'World']
         return render_template('index.html', stops=stops)
 
     if request.method == "POST":
         return redirect(url_for('search'))
+
+@app.route("/student", methods=["POST"])
+def student():
+
+    if request.method == "POST":
+
+        print 'Before', request.form
+        student_name = 'Bobby Tables' # TODO: where's it coming from?  request.form['']
+        print 'Name:', student_name
+        route = request.form['transport_line']
+        print 'Route:', route
+        direction = request.form['inbound']
+        print 'Direction:', direction
+        store_student(student_name, route, direction)
+        return 'OK'
+
+def store_student(student_name, route, direction):
+    
+    # TODO: sanitize input!!!
+
+    conn = get_db_conn()
+    conn.execute("INSERT INTO students (name, route, direction, delay) VALUES ('{}', '{}', '{}', None)".format(student_name, route, direction, ))
+
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
